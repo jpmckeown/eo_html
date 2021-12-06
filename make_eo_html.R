1114eeaa906f72b95dcbc24e9179aa4dd25be85d
jpmckeown <j.p.mckeown@icloud.com>
2021-12-06 20:06:43 +0000

    began page layout and carousel

--- make_eo_html.R
+++ make_eo_html.R
@@ -2,8 +2,14 @@
 # menu will be made from country list
 # file.append doesnt add newline between files 
 # so fragments after last line need extra newline.
-page <- 'country.html'
+page <- 'AFG.html'
 file.create(page)
 file.append(page, 'start.htm')
-file.append(page, 'menu.htm')
+file.append(page, 'select_by_continent.htm')
+
+out <- file(page, open='a')
+cat('<h1>Afghanistan</h1>', sep='\n', file=out)
+
+close(out)
+
 file.append(page, 'end.htm')
--- make_navbar.R
+++ make_navbar.R
@@ -1,5 +1,6 @@
 # make nav Continents dropdown as fragment htm
-# library(tidyverse)
+library(tidyverse)
+library(textutils)
 
 # temporary, get EO data from previously assembled dataframe
 # load(file='data/eo.Rda')
@@ -13,12 +14,11 @@ cat('<nav class="continent">', sep='\n', file=out)
 continents <- c('Africa', 'Americas', 'Asia', 'Europe', 'Oceania') 
 
 for (c in seq_along(continents)) {
-  #print(continents[c])
   # begin dropdown for a Continent
-  cat('  <div class="dropdown">', sep='\n', file=out)
-  cat(paste0('  <button class="dropbtn">', continents[c]), sep='\n', file=out)
-  cat('  </button>', sep='\n', file=out)
-  cat('    <div class="dropdown-content">', sep='\n', file=out)
+  cat(paste0(spaces(2), '<div class="dropdown">'), sep='\n', file=out)
+  cat(paste0(spaces(4), '<button class="dropbtn">', continents[c]), sep='\n', file=out)
+  cat(paste0(spaces(4), '</button>'), sep='\n', file=out)
+  cat(paste0(spaces(4), '<div class="dropdown-content">'), sep='\n', file=out)
   
   # loop Countries in that continent
   thisContinent <- eo %>% 
@@ -29,12 +29,11 @@ for (c in seq_along(continents)) {
   for (k in 1:nrow(thisContinent)) {
     iso3c <- thisContinent$iso3c[k]
     Country <- thisContinent$Country[k] 
-    # print(paste0("<a href='", iso3c, ".html'>", Country, "</a>"))
-    cat(paste0("      <a href='", iso3c, ".html'>", Country, "</a>"), sep='\n', file=out)
+    cat(paste0(spaces(6), "<a href='", iso3c, ".html'>", Country, "</a>"), sep='\n', file=out)
   }
   
-  cat('    </div>', sep='\n', file=out)    
-  cat('  </div>', sep='\n', file=out)    
+  cat(paste0(spaces(4), '</div>'), sep='\n', file=out)  # dropdown-content
+  cat(paste0(spaces(2), '</div>'), sep='\n', file=out)  # dropdown
 }
-cat('</nav>', sep='\n', file=out)    
+cat('</nav>', sep='\n', file=out)  
 close(out)
