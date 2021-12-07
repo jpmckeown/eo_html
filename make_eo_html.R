f961686d695801077c0d82c3a72f59604622c7f5
jpmckeown <j.p.mckeown@icloud.com>
2021-12-07 09:10:34 +0000

    fragments moved back to subfolder

--- make_eo_html.R
+++ make_eo_html.R
@@ -4,12 +4,12 @@
 # so fragments after last line need extra newline.
 page <- 'AFG.html'
 file.create(page)
-file.append(page, 'start.htm')
-file.append(page, 'select_by_continent.htm')
+file.append(page, 'fragment/start.htm')
+file.append(page, 'fragment/select_by_continent.htm')
 
 out <- file(page, open='a')
 cat('<h1>Afghanistan</h1>', sep='\n', file=out)
 
 close(out)
 
-file.append(page, 'end.htm')
+file.append(page, 'fragment/end.htm')
