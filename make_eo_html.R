f4e99154b3123204fe6837159e6c52a5ab87628f
jpmckeown <j.p.mckeown@icloud.com>
2021-12-10 16:29:50 +0000

    begin loop to make 1 htm for each country

--- make_eo_html.R
+++ make_eo_html.R
@@ -2,14 +2,26 @@
 # menu will be made from country list
 # file.append doesnt add newline between files 
 # so fragments after last line need extra newline.
-page <- 'AFG.html'
-file.create(page)
-file.append(page, 'fragment/start.htm')
-file.append(page, 'fragment/select_by_continent.htm')
+library(dplyr)
+#load('data/eo.Rda') 
+startCountry <- 1 #nrow(eo) -10
+endCountry <- 10 #nrow(eo)
 
-out <- file(page, open='a')
-cat('<h1>Afghanistan</h1>', sep='\n', file=out)
-
-close(out)
-
-file.append(page, 'fragment/end.htm')
+for (k in startCountry:endCountry) {
+  
+  country <- eo$Country[k]
+  continent <- as.character(eo %>% filter(iso3c == eo[k,'iso3c']) %>% select(Continent))
+  iso2c <- eo$iso2c[k]
+  iso3c <- eo$iso3c[k]
+  page <- paste0('result/', iso3c, '.html')
+  file.create(page)
+  file.append(page, 'fragment/start.htm')
+  file.append(page, 'fragment/select_by_continent.htm')
+  
+  out <- file(page, open='a')
+  cat('<h1>Afghanistan</h1>', sep='\n', file=out)
+  
+  close(out)
+  
+  file.append(page, 'fragment/end.htm')
+}
