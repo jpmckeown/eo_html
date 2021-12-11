82979a50600b1c3e9abe134e0e8dec6ac13133a6
jpmckeown <j.p.mckeown@icloud.com>
2021-12-11 11:14:14 +0000

    data source popups working!

---
+++ extra.R
@@ -0,0 +1,13 @@
+#load('data/eo.Rda') 
+#load('data/imgdata.Rda') 
+indicators <- read_csv("data/indicators.csv")
+description <- c(
+  'Population size in 2018, from United Nations DESA World Population Prospects 2019 Revision',
+  'Maximum sustainable population based on the country's 2018 natural resource availability and economic activity',
+  'World Bank 2020. A positive % means population is increasing; negative means population is decreasing',
+  'Estimated contraceptive use among women 15-49 years old from United Nations DESA, World Population Prospects 2019 Revision',
+  'Number of species critically endangered, endangered, and vulnerable, IUCN July 2019',
+  'A = Excellent\nB = Above Average\nC = Average\nD = Below Average\nF = Poor',
+  'Comments by Earth Overshoot'
+)
+indicators <- cbind(indicators, description)
--- make_eo_html.R
+++ make_eo_html.R
@@ -3,9 +3,6 @@
 # file.append doesnt add newline between files 
 # so fragments after last line need extra newline.
 library(tidyverse)
-#load('data/eo.Rda') 
-#load('data/imgdata.Rda') 
-indicators <- read_csv("data/indicators.csv")
 
 startCountry <- 1 
 endCountry <- 10 #nrow(eo)
@@ -39,9 +36,11 @@ for (k in startCountry:endCountry) {
     if (i == 3 || i == 4) {
       value <- paste(value, '%')
     }
-    label <- as.character(indicators[i, 1])
-    longLabel <- as.character(indicators[i, 2])
-    cat(paste0('    <div class="cell ', label, '"><h3>', value, '</h3>\n<h2>', longLabel, '</h2></div>'), sep='\n', file=out)
+    label <- indicators[i, 1]
+    longLabel <- indicators[i, 2]
+    popup <-  indicators[i, 3]
+    cat(paste0('    <div class="cell ', label, '"><h3>', value, '</h3>'), sep='\n', file=out)
+    cat(paste0('<h2><a href="#" data-tooltip data-tooltip-label="', popup, '">', longLabel, '</a></h2></div>'), sep='\n', file=out)
   }
   cat('  </section>', sep='\n', file=out)
 
@@ -54,7 +53,8 @@ for (k in startCountry:endCountry) {
   
   # Photo carousel
   cat('  <section class="slideshow-container fade">', sep='/n', file=out)
-    
+  cat(paste0('<h4>Photo gallery for ', country, '</h4>'), sep='/n', file=out)
+  
   # identify number of images for each country
   numPhotos <- eo$Freq[k]
   #print(paste(k, iso2c, numPhotos, country))
@@ -65,7 +65,7 @@ for (k in startCountry:endCountry) {
   if (numPhotos>0) {
     IDs <- imgdata[imgdata$iso2c == eo$iso2c[k],]$ID
     strIDs <- toString(IDs)
-    #print(paste(iso2c, numPhotos, country, strIDs))
+    print(paste(iso2c, numPhotos, country, strIDs))
     
     for (ph in seq_along(IDs)) {
       # filename
