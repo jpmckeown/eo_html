19a574aaf78d6cacdb33ddc29e58aaf8aeb95413
jpmckeown <j.p.mckeown@icloud.com>
2021-12-12 12:55:35 +0000

    make HTML tidier and comment ends of blocks

--- make_eo_html.R
+++ make_eo_html.R
@@ -6,7 +6,7 @@ library(tidyverse)
 library(stringi)
 
 startCountry <- 1 
-endCountry <- nrow(eo)
+endCountry <- 12 # nrow(eo)
 
 for (k in startCountry:endCountry) {
   
@@ -23,15 +23,15 @@ for (k in startCountry:endCountry) {
   out <- file(page, open='a')
   
   # Country name
-  cat('  <section class="country">', sep='', file=out)
-  cat(paste0('<h1>', country, '</h1></section>'), sep='\n', file=out)
+  cat('<section class="country">', sep='', file=out)
+  cat(paste0('<h1>', country, '</h1></section>\n'), sep='\n', file=out)
   
   # Flag
-  cat('  <section class="flag">', sep='', file=out)
-  cat(paste0('<img src="../flag/', lowiso2c, '.svg" height="120"></section>'), sep='\n', file=out)
+  cat('<section class="flag">', sep='', file=out)
+  cat(paste0('<img src="../flag/', lowiso2c, '.svg" height="120"></section>\n'), sep='\n', file=out)
   
   # Data grid
-  cat('  <section class="grid">', sep='\n', file=out)
+  cat('<section class="grid">  <!-- data items are 1 row of FlexBox -->\n', sep='\n', file=out)
   for (i in 1:6) {
     if (i == 6 && is.na(eo[k, i+2])) {
       value <- 'N/A'
@@ -47,23 +47,30 @@ for (k in startCountry:endCountry) {
     longLabel <- as.character(indicators[i, 2])
     popup <-  as.character(indicators[i, 3])
 
-    cat(paste0('    <div class="cell ', codeLabel, '"><h3>', value, '</h3>'), sep='\n', file=out)
-    cat(paste0('<h2><a id="', codeLabel, '_" href="#" data-tooltip data-tooltip-label="', popup, '">', longLabel, '</a></h2>'), sep='\n', file=out)
-    cat(paste0('<div class="icon" id="', codeLabel, '"><i class="fas fa-plus-circle"></i></div></div>'), sep='\n', file=out)
-  }
-  cat('  </section>', sep='\n', file=out)
+    cat(paste0('<div class="cell ', codeLabel, '">'), sep='\n', file=out)
+    cat(paste0('  <h3>', value, '</h3>'), sep='\n', file=out)
+    
+    cat(paste0('  <h2><a id="', codeLabel, '_" href="#" data-tooltip data-tooltip-label="', popup, '">', longLabel, '</a></h2>'), sep='\n', file=out)
+    
+    cat(paste0('  <div class="icon" id="', codeLabel, '"><i class="fas fa-plus-circle"></i></div>'), sep='\n', file=out)
+    cat('</div>  <-- cell ends -->\n', sep='\n', file=out)
+  } # end of 6 data items 
+  
+  cat('</section>\n', sep='\n', file=out)
 
   # Comment
   commentPath <- paste0('comment/', iso2c, '.txt')
   txtC <- readChar(commentPath, file.info(commentPath)$size)
   txtC <- stri_encode(txtC, '', 'UTF-8')
   txtD <- paste0('<p>', gsub("[\r\n]+", "</p><p>", txtC), '</p>')
-  txtE <- gsub("</p><p></p>", "</p>", txtD)
-  txtF <- gsub(" </p>", "</p>", txtE)
+  txtD <- gsub("<p><p>", "<p>", txtD)
+  txtD <- gsub(" </p>", "</p>", txtD)
+  txtD <- gsub("</p><p></p>", "</p>", txtD)
+  txtD <- gsub("</p></p>", "</p>", txtD)
 
-  cat('  <section class="comment">', sep='', file=out)
+  cat('<section class="comment">', sep='\n', file=out)
   cat('<h4>Comments on country</h4>', sep='\n', file=out)
-  cat(paste0('   <p>', txtF, '</p></section>'), sep='\n', file=out)
+  cat(paste0('<p>', txtD, '</p></section>\n'), sep='\n', file=out)
   
   # Photo gallery: title, caption, and carousel
   # identify number of images for each country
@@ -78,9 +85,9 @@ for (k in startCountry:endCountry) {
     print(paste(iso2c, numPhotos, country, strIDs))
     
     cat('<section class="gallery">', sep='\n', file=out)
-    cat(paste0('  <h4>Photo gallery for ', country, '</h4>'), sep='\n', file=out)
+    cat(paste0('<h4>Photo gallery for ', country, '</h4>'), sep='\n', file=out)
     
-    cat('  <div class="slideshow-container fade">', sep='\n', file=out)
+    cat('<div class="slideshow-container fade">\n', sep='\n', file=out)
   
     for (ph in seq_along(IDs)) {
       # filename
@@ -91,18 +98,18 @@ for (k in startCountry:endCountry) {
         img <- paste0('../photo/', eo[k,1], '_', IDs[ph], '.png')
         imgpath <- paste0('photo/', eo[k,1], '_', IDs[ph], '.png')
       }
-      if (!file.exists(imgpath)) {
-        img <- paste0('../photo/', eo[k,1], '_', IDs[ph], '.svg')
-      }
+      # if (!file.exists(imgpath)) {
+      #   img <- paste0('../photo/', eo[k,1], '_', IDs[ph], '.svg')
+      # }
 
       # fix caption encoding
       caption <- stri_encode(captions[ph], '', 'UTF-8')
       
-      cat('<div class="Containers">', sep='\n', file=out)
+      cat('<div class="photo-container">', sep='\n', file=out)
       cat(paste0('<div class="caption">', caption, '</div>'), sep='\n', file=out)
-      cat(paste0('<img src="', img, '" style="width:100%">'), sep='\n', file=out)
+      cat(paste0('<img src="', img, '">'), sep='\n', file=out)
       cat(paste0('<div class="attribution">', attributions[ph], '</div>'), sep='\n', file=out)
-      cat('</div>', sep='\n', file=out)
+      cat('</div> <!-- photo-container -->\n', sep='\n', file=out)
     }
     
     if(numPhotos > 1) {
