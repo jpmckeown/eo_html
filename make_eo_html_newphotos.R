6bb77095b2dd1c6bb4cfdac792f21d434fa673d2
jpmckeown <j.p.mckeown@icloud.com>
2022-02-05 09:11:00 +0000

    begin changes adding GDP

--- make_eo_html_newphotos.R
+++ make_eo_html_newphotos.R
@@ -24,7 +24,8 @@ description <- c(
   "Estimated modern contraceptive use among women 15-49 years old.\nPoor 0-20%\nBelow Average 20-40%\nAverage 40-60%\nAbove Average 60-80%\nExcellent 80-100%", 
   "Number of species critically endangered, endangered, or vulnerable. IUCN Red List of Threatened Species.", 
   "Maximum sustainable population based on the country's 2017 natural resource availability and economic activity.", 
-  "A positive % means population is increasing; negative means population is decreasing.", 
+  "Annual rate: a positive % means population is increasing; negative means population is decreasing.", 
+  "Per Capita GDP in U.S. Dollars. A high figure indicates high consumption of resources per person. A low figure indicates low consumption of resources per person.", 
   "A = Excellent\nB = Above Average\nC = Average\nD = Below Average\nF = Poor", 
   "Comments by Earth Overshoot"
 )
